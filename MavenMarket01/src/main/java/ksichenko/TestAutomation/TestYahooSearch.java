4e15194386c7b8b14dd779868f9ef8cbaad87c21
kostyantins <kostyantins@mail.ru>
2016-05-27 11:40:08 +0300

    TarasPractice07 by ksichenko 24.05.2016 with correctation

--- MavenMarket01/src/main/java/ksichenko/TestAutomation/TestYahooSearch.java
+++ MavenMarket01/src/main/java/ksichenko/TestAutomation/TestYahooSearch.java
@@ -1,7 +1,37 @@
 package ksichenko.TestAutomation;
 
-/**
- * Created by macbook on 24.05.16.
- */
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class TestYahooSearch {
+
+
+    private final WebDriver driver = new FirefoxDriver();
+
+    @Test
+    public void testYahooSearch() {
+
+        driver
+                .get("https://www.yahoo.com");
+
+        driver
+                .findElement(By.xpath(".//*[@id='uh-search-box']"))
+                .sendKeys("test automation");
+
+        driver
+                .findElement(By.xpath(".//*[@id='uh-search-button']"))
+                .click();
+
+        String actualTitle = "Test Automation Software | Atlassian.com";
+        String expectedTitle = driver.getTitle();
+
+        Assert
+                .assertTrue(driver.getTitle()
+                .contains("test automation"));
+    }
 }
+
+
